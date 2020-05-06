cc96c68b7583feae9be298286cdd1550d73ddf67
Victorhlab <Victorhlab@gmail.com>
2020-05-06 16:44:37 +0200

    Demonstrate component modularity and how external data can be used in components

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
+
+class DishDetail extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    renderComments(commentsArr){
+        var commentSection = (<div></div>);
+
+        if(commentsArr != null)
+        {
+            var comments = commentsArr.map( (item) => {
+                    return(
+                        <div key={item.id} >
+                            <li className="list-unstyled">
+                                <p>{item.comment}</p>
+                                <p>--{item.author} {item.date}</p>
+                            </li>
+                        </div>
+                    )
+                }
+            );
+
+            commentSection = (
+                <div>
+                    <h4>Comments</h4>
+                    {comments}
+                </div>
+            );  
+        }
+
+        return commentSection;
+    }
+
+    renderDish(dish) {
+        let dishToRender;
+
+        if( dish != null)
+        {
+            dishToRender = (
+                <div>
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle> {dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            );
+        }
+        else {
+            dishToRender = (<div></div>)
+        }
+
+        return dishToRender;
+    }
+
+    render() {
+
+        if(this.props.dish != null) {
+            var dishdetail = this.renderDish(this.props.dish);
+            var comments   = this.renderComments(this.props.dish.comments);
+        }
+            
+        return( <div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    {dishdetail}
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <ul>
+                        {comments}
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+           
+        );  
+    }
+}
+
+export default DishDetail;
